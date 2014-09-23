f13077fc5a3e81109a7a8166497b07096739b582
buihaduong <buihaduong@yahoo.com.vn>
2014-09-24 00:07:29 +0700

    Untrack Debug

--- tismartbin/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"D:/ti/TivaWare_C_Series-2.1.0.12573/examples/boards/ek-tm4c123gxl/project0/ccs/../../../../../driverlib/ccs/Debug/driverlib.lib"
-
